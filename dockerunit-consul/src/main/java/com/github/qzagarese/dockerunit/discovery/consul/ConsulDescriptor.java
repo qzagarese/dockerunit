5a48947dc1f5a516c669662e765ef20e174a7cfa
Quirino Zagarese <quirino.zagarese@gmail.com>
2019-02-18 18:46:33 +0000

    [ISSUE-12] Adding -Dconsul.dns.off flag to allow execution on Mac

--- dockerunit-consul/src/main/java/com/github/qzagarese/dockerunit/discovery/consul/ConsulDescriptor.java
+++ dockerunit-consul/src/main/java/com/github/qzagarese/dockerunit/discovery/consul/ConsulDescriptor.java
@@ -1,12 +1,5 @@
 package com.github.qzagarese.dockerunit.discovery.consul;
 
-import static com.github.qzagarese.dockerunit.discovery.consul.ConsulDiscoveryConfig.*;
-import static com.github.qzagarese.dockerunit.discovery.consul.ConsulDiscoveryConfig.DOCKER_BRIDGE_IP_PROPERTY;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import com.github.dockerjava.api.command.CreateContainerCmd;
 import com.github.dockerjava.api.model.ExposedPort;
 import com.github.dockerjava.api.model.Ports;
@@ -14,6 +7,14 @@ import com.github.dockerjava.api.model.Ports.Binding;
 import com.github.qzagarese.dockerunit.annotation.ContainerBuilder;
 import com.github.qzagarese.dockerunit.annotation.Image;
 import com.github.qzagarese.dockerunit.annotation.Named;
+import com.github.qzagarese.dockerunit.discovery.consul.annotation.UseConsulDns;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+
+import static com.github.qzagarese.dockerunit.discovery.consul.ConsulDiscoveryConfig.*;
 
 @Named("consul")
 @Image("consul:1.0.0")
@@ -23,7 +24,10 @@ public class ConsulDescriptor {
 	static final int CONSUL_DNS_PORT = 8600;
 	static final int CONSUL_PORT = 8500;
 
-	@ContainerBuilder
+    private static final Logger logger = Logger.getLogger(ConsulDescriptor.class.getSimpleName());
+
+
+    @ContainerBuilder
 	public CreateContainerCmd setup(CreateContainerCmd cmd) {
 		List<ExposedPort> ports = new ArrayList<>(Arrays.asList(cmd.getExposedPorts()));
         ExposedPort dnsPort = ExposedPort.udp(CONSUL_DNS_PORT);
@@ -37,12 +41,19 @@ public class ConsulDescriptor {
             bindings = new Ports();
         }
         
-        int dnsBridgePort = Integer.parseInt(System.getProperty(CONSUL_DNS_PORT_BRIDGE_BINDING, 
-                CONSUL_DNS_PORT_BRIDGE_BINDING_DEFAULT));
+        String consulDnsFlagOff = System.getProperty(CONSUL_DNS_OFF_PROPERTY, CONSUL_DNS_OFF_DEFAULT);
 
-        bindings.bind(dnsPort, Binding.bindIpAndPort(
-                System.getProperty(DOCKER_BRIDGE_IP_PROPERTY, DOCKER_BRIDGE_IP_DEFAULT), 
-                dnsBridgePort));
+        if(consulDnsFlagOff == null) {
+            int dnsBridgePort = Integer.parseInt(System.getProperty(CONSUL_DNS_PORT_BRIDGE_BINDING,
+                    CONSUL_DNS_PORT_BRIDGE_BINDING_DEFAULT));
+
+            bindings.bind(dnsPort, Binding.bindIpAndPort(
+                    System.getProperty(DOCKER_BRIDGE_IP_PROPERTY, DOCKER_BRIDGE_IP_DEFAULT),
+                    dnsBridgePort));
+        } else {
+            logger.warning("Consul dns has been disabled. Usages of @" + UseConsulDns.class.getSimpleName() + " will not sort any effect.");
+        }
+        
         bindings.bind(consulPort, Binding.bindPort(8500));
 
         return cmd.withExposedPorts(ports)
