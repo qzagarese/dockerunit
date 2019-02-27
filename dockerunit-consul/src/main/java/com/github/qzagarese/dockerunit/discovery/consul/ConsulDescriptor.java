f34436bcab039e7d0e294f59e111d2d399ea659c
Quirino Zagarese <quirino.zagarese@gmail.com>
2019-02-27 16:49:43 +0000

    [ISSUE-12] Addressing review comments

--- dockerunit-consul/src/main/java/com/github/qzagarese/dockerunit/discovery/consul/ConsulDescriptor.java
+++ dockerunit-consul/src/main/java/com/github/qzagarese/dockerunit/discovery/consul/ConsulDescriptor.java
@@ -21,45 +21,45 @@ import static com.github.qzagarese.dockerunit.discovery.consul.ConsulDiscoveryCo
 @Image("consul:1.0.0")
 public class ConsulDescriptor {
 
-	
-	static final int CONSUL_DNS_PORT = 8600;
-	static final int CONSUL_PORT = 8500;
+
+    static final int CONSUL_DNS_PORT = 8600;
+    static final int CONSUL_PORT = 8500;
 
     private static final Logger logger = Logger.getLogger(ConsulDescriptor.class.getSimpleName());
 
 
     @ContainerBuilder
-	public CreateContainerCmd setup(CreateContainerCmd cmd) {
-		List<ExposedPort> ports = new ArrayList<>(Arrays.asList(cmd.getExposedPorts()));
+    public CreateContainerCmd setup(CreateContainerCmd cmd) {
+        List<ExposedPort> ports = new ArrayList<>(Arrays.asList(cmd.getExposedPorts()));
 
         ExposedPort consulPort = ExposedPort.tcp(CONSUL_PORT);
         ports.add(consulPort);
-        
+
         Ports bindings = cmd.getPortBindings();
         if (bindings == null) {
             bindings = new Ports();
         }
-        
+
         Optional<String> disableDnsFlag = Optional.ofNullable(System.getProperty(CONSUL_DNS_OFF_PROPERTY, CONSUL_DNS_OFF_DEFAULT));
 
         if(!disableDnsFlag.isPresent()) {
             activateDns(ports, bindings);
         } else {
-            logger.warning("Consul dns has been disabled. Usages of @" + UseConsulDns.class.getSimpleName() + " will not sort any effect.");
+            logger.warning("Consul DNS has been disabled. Usages of @" + UseConsulDns.class.getSimpleName() + " will be ignored.");
         }
-        
+
         bindings.bind(consulPort, Binding.bindPort(8500));
 
         return cmd.withExposedPorts(ports)
-            .withPortBindings(bindings)
-            .withCmd("agent", "-dev", "-client=0.0.0.0", "-enable-script-checks");
+                .withPortBindings(bindings)
+                .withCmd("agent", "-dev", "-client=0.0.0.0", "-enable-script-checks");
 
-	}
+    }
 
     private void activateDns(List<ExposedPort> ports, Ports bindings) {
         ExposedPort dnsPort = ExposedPort.udp(CONSUL_DNS_PORT);
         ports.add(dnsPort);
-        
+
         int dnsBridgePort = Integer.parseInt(System.getProperty(CONSUL_DNS_PORT_BRIDGE_BINDING,
                 CONSUL_DNS_PORT_BRIDGE_BINDING_DEFAULT));
 
